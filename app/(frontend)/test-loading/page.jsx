b718e8e47a139e9ff0813868d5e606b47d7b31bf
garvAtGit <garvv18@gmail.com>
2026-06-27 19:47:48 +0530

    chore: remove 4 obsolete routes and uninstall 5 outdated/redundant dependencies

--- app/(frontend)/test-loading/page.jsx
+++
@@ -1,5 +0,0 @@
-import Loading from "../loading";
-
-export default function TestLoadingPage() {
-    return <Loading />;
-}
--- app/(frontend)/thank-you/page.jsx
+++
@@ -1,9 +0,0 @@
-// app/thank-you/page.jsx
-export default function ThankYou() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h2>🎉 Payment Successful</h2>
-      <p>Your access will be granted soon.</p>
-    </div>
-  );
-}
