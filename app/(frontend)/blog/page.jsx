7a6668eb860211fbb7e89c763006f3f2d080f5c3
garvAtGit <garvv18@gmail.com>
2026-07-06 01:11:47 +0530

    Fix relative paths for payload.config to resolve build error

--- app/(frontend)/blog/page.jsx
+++ app/(frontend)/blog/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getPayload } from 'payload';
-import configPromise from '@/payload.config';
+import configPromise from '../../../payload.config';
 import Link from 'next/link';
 import { Cinzel, Manrope } from "next/font/google";
 
